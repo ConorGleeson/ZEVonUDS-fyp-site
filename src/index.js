bdc517cf0c81344766428ebbe8432b7f34e4ca3b
Conor Gleeson <conorgleeson20093384@gmail.com>
2024-01-19 21:22:56 +0000

    fixed refresh issue

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import HomePage from './pages/homePage/homePage';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/system';  // Update this import
 import reportWebVitals from './reportWebVitals';
-import { Navigate } from 'react-router-dom';
-
-
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider>
       <Router>
         <Routes>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="*" element={<Navigate to="/home" replace />} />
+          <Route path="/ZEVonUDS-fyp-site" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/ZEVonUDS-fyp-site" replace />} />
           {/* Add more Route components for other paths */}
         </Routes>
       </Router>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

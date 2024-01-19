676e58ef24521da1cfae7d54a4cc7ffa3782ff1c
Conor Gleeson <conorgleeson20093384@gmail.com>
2024-01-19 21:03:49 +0000

    inital commit

---
+++ src/pages/homePage/homePage.js
@@ -0,0 +1,12 @@
+import {React} from 'react';
+import { Container, Typography } from '@mui/material';
+
+const MainPage = () => {
+  return (
+    <Container sx={{alignContent: 'center', alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', padding:'2em 0'}}>
+      <Typography variant="h1">ZEVonUDS</Typography>
+    </Container>
+  );
+}
+
+export default MainPage; 
